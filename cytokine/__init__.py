bcd453bc3f57f30f312f9e7fd320549f2ab06e6d
Micah Johnston <micah@glowcoil.com>
2018-11-12 09:11:57 -0600

    first commit

---
+++ cytokine/__init__.py
@@ -0,0 +1,13 @@
+bl_info = {
+    "name": "Cytokine",
+    "author": "Micah Johnston",
+    "version": (0, 0, 1),
+    "blender": (2, 79, 0),
+    "category": "Object"
+}
+
+def register():
+    pass
+
+def unregister():
+    pass
---
+++ deploy.sh
@@ -0,0 +1,3 @@
+export C_INCLUDE_PATH=/System/Library/Frameworks/Python.framework/Headers
+/Applications/blender-2.79.0-git20170809.f2728939df3-x86_64/blender.app/Contents/Resources/2.79/python/bin/python3.5m setup.py build_ext
+rsync -rv --delete --exclude=.DS_Store --exclude=.git --exclude=*.blend1 --exclude=*.blend2 --exclude=*.swp --exclude=*.swo cytokine ~/Library/Application\ Support/Blender/2.79/scripts/addons/
