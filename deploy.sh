6410b676be8c17243c75ea6bd700a6da6e6f028a
Micah Johnston <micah@glowcoil.com>
2018-11-30 19:11:28 -0600

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 export C_INCLUDE_PATH=/System/Library/Frameworks/Python.framework/Headers
-/Applications/blender-2.79.0-git20170809.f2728939df3-x86_64/blender.app/Contents/Resources/2.79/python/bin/python3.5m setup.py build_ext
-rsync -r --delete --exclude=.DS_Store --exclude=.git --exclude=*.sh --exclude=*.c cytokine ~/Library/Application\ Support/Blender/2.79/scripts/addons/
+python setup.py build_ext
+rsync -r --delete --exclude=.DS_Store --exclude=.git --exclude=*.sh --exclude=*.c cytokine ~/Library/Application\ Support/Blender/2.80/scripts/addons/
